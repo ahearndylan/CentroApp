10743f8af884c3a1b3cda6fadecfac7f51afbab2
ahearndylan <dahearn2021@gmail.com>
2024-09-28 16:07:51 -0400

    Initial commit

---
+++ Centro/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
+    path('', user_views.home, name='home'),  # Home page
+]
