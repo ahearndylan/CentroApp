bfb7bc8e831b639e09c5dd95e0d2f4247ae41edf
ahearndylan <dahearn2021@gmail.com>
2024-10-24 21:26:09 -0400

    Updated UI

--- Centro/urls.py
+++ Centro/urls.py
@@ -5,7 +5,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('register/', user_views.register, name='register'),
 
     path('', user_views.home, name='home'),  # Home page
